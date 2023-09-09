9ef8e7da681ddec39f3ca94a64ebb22841a23300
Dimboba <lazarkovdmitry@yandex.ru>
2023-09-09 23:24:36 +0300

    fix: public constructors

--- src/main/java/laz/dimboba/library/Exceptions/NotFoundException.java
+++ src/main/java/laz/dimboba/library/Exceptions/NotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException{
-    NotFoundException(String message){
+    public NotFoundException(String message){
         super(message);
     }
 }
