df3a7420b97b53dcb96fddaf7f6f7d9e426b19f9
Dimboba <lazarkovdmitry@yandex.ru>
2023-09-09 23:53:46 +0300

    feat: CustomerServiceJPAImpl

--- src/main/java/laz/dimboba/library/dao/CustomerRepository.java
+++ src/main/java/laz/dimboba/library/dao/CustomerRepository.java
@@ -3,7 +3,12 @@ package laz.dimboba.library.dao;
 import laz.dimboba.library.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    List<Customer> findAllByFirstNameContainingIgnoreCaseAAndLastNameContainingIgnoreCase(
+            String firstName,
+            String lastName
+            );
 }
--- src/main/java/laz/dimboba/library/service/jpa/CustomerServiceJPAImpl.java
+++ src/main/java/laz/dimboba/library/service/jpa/CustomerServiceJPAImpl.java
@@ -1,30 +1,49 @@
 package laz.dimboba.library.service.jpa;
 
+import laz.dimboba.library.Exceptions.AlreadyExistException;
+import laz.dimboba.library.Exceptions.NotFoundException;
+import laz.dimboba.library.dao.CustomerRepository;
 import laz.dimboba.library.entity.Customer;
 import laz.dimboba.library.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
+@AllArgsConstructor
 public class CustomerServiceJPAImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
     @Override
     public Customer getCustomer(UUID customerId) {
-        return null;
+        return customerRepository.findById(customerId)
+                .orElseThrow(() -> new NotFoundException("There is no customer with id = " + customerId));
     }
 
     @Override
     public List<Customer> getCustomersByFirstNameAndLastName(String firstName, String lastName) {
-        return null;
+        return customerRepository.findAllByFirstNameContainingIgnoreCaseAAndLastNameContainingIgnoreCase(
+                firstName,
+                lastName
+        );
     }
 
     @Override
     public Customer saveCustomer(Customer customer) {
-        return null;
+        if(customerRepository.existsById(customer.getId())){
+            throw new AlreadyExistException("Customer with id = " + customer.getId() + " already exists");
+        }
+        return customerRepository.save(customer);
     }
 
     @Override
     public Customer updateCustomer(Customer customer) {
-        return null;
+        if(!customerRepository.existsById(customer.getId())){
+            throw new NotFoundException("Customer with id = " + customer.getId() + " not found");
+        }
+        return customerRepository.save(customer);
     }
 }
