ed74c848774e70b8e9b4dbca4640b45cc4387a48
Dimboba <lazarkovdmitry@yandex.ru>
2023-09-09 23:36:32 +0300

    feat: impl of BookRepository

--- src/main/java/laz/dimboba/library/dao/BookRepository.java
+++ src/main/java/laz/dimboba/library/dao/BookRepository.java
@@ -3,7 +3,12 @@ package laz.dimboba.library.dao;
 import laz.dimboba.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+
+    List<Book> findAllByTitleAndAuthor(String title, String author);
+
 }
--- src/main/java/laz/dimboba/library/service/jpa/BookServiceJPAImpl.java
+++ src/main/java/laz/dimboba/library/service/jpa/BookServiceJPAImpl.java
@@ -1,5 +1,6 @@
 package laz.dimboba.library.service.jpa;
 
+import laz.dimboba.library.Exceptions.AlreadyExistException;
 import laz.dimboba.library.Exceptions.NotFoundException;
 import laz.dimboba.library.dao.BookRepository;
 import laz.dimboba.library.entity.Book;
@@ -25,16 +26,22 @@ public class BookServiceJPAImpl implements BookService {
 
     @Override
     public List<Book> getBooksByTitleAndAuthor(String title, String author) {
-        return null;
+        return bookRepository.findAllByTitleAndAuthor(title, author);
     }
 
     @Override
     public Book saveBook(Book book) {
-        return null;
+        if(bookRepository.existsById(book.getId())){
+            throw new AlreadyExistException("Book with id = " + book.getId() + " already exists");
+        }
+        return bookRepository.save(book);
     }
 
     @Override
     public Book updateBook(Book book) {
-        return null;
+        if(!bookRepository.existsById(book.getId())){
+            throw new NotFoundException("Book with id = " + book.getId() + " not found");
+        }
+        return bookRepository.save(book);
     }
 }
