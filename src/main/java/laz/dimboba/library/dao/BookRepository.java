343c57e7a7ead666809947ae6dd61eacfb0b6258
Dimboba <lazarkovdmitry@yandex.ru>
2023-09-10 12:10:20 +0300

    refactor: spaces

--- src/main/java/laz/dimboba/library/dao/BookRepository.java
+++ src/main/java/laz/dimboba/library/dao/BookRepository.java
@@ -2,13 +2,11 @@ package laz.dimboba.library.dao;
 
 import laz.dimboba.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
-
-
     List<Book> findAllByTitleContainingIgnoreCaseAndAuthorContainingIgnoreCase(String title, String author);
-
 }
